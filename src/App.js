4edede92814c47e40e7857370d900b6c2069dda4
LarisaShatil <larisa.shat@ukr.net>
2021-10-14 00:07:15 +0300

    final

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
 import { Component } from 'react';
-import { v4 as uuid } from 'uuid';
 import ContactList from './components/ContactList/ContactList';
 import Container from './components/Container/Container';
+import ContactForm from './components/ContactForm/ContactForm';
+import Filter from './components/Filter/Filter';
 
 class App extends Component {
   state = {
@@ -17,92 +18,65 @@ class App extends Component {
     number: ''
   };
 
-//writing data from input
-  handleChange = (e) => {
-    console.log(e.target.value)
+  addNewContacts = data => {
+    this.setState((prevState) => {
+      if (prevState.contacts.some(contact => contact.name.toLowerCase().includes(data.name.toLowerCase()))) {
+        return alert(`${data.name} is already in your contacts`)
+      }
+      return {
+        contacts: [data, ...prevState.contacts]
+      }
+    });
+  };
+
+
+  deleteContact = id => {
+    // console.log('DELETE')
+    this.setState(prev => ({
+      contacts: prev.contacts.filter(contact => contact.id !== id),
+    }))
+  };
+
+  filterName = e => {
+    // console.log('FILTER')
     this.setState({
-      [e.target.name]: e.target.value,
+      filter: e.currentTarget.value,
     })
   };
 
-  //collecting and forwarding data to obj
-  handleSubmit = (e) => {
-    e.preventDefault();
-    const contact = {
-      id: uuid(),
-      name: this.state.name,
-      number: this.state.number
-    };
- 
-    this.setState((prevState) => {
-      return {
-        contacts: [ contact, ...prevState.contacts]
-      }
-    });
-    this.resetForm();
-   };
+  getFilteredContacts = () => {
+    const { contacts, filter } = this.state;
+    const nameToLowerCase = filter.toLowerCase();
 
-  // handleSubmit = data => {
-  //   this.setState(prevState => {
-  //     if (
-  //       prevState.contacts.some(contact => contact.name.includes(data.name))
-  //     ) {
-  //       return alert(`${data.name} is already in contacts!`);
-  //     }
-
-  //     return { contacts: [data, ...prevState.contacts] };
-  //   });
-  // };
-
-  //clears inputs. Used in handleSubmit
-  resetForm = () => {
-    this.setState({ name: '', number: '' })
+    return (
+      contacts.filter(contact =>
+        contact.name.toLowerCase().includes(nameToLowerCase)
+      )
+    )
   };
 
-  nameId = uuid();
-  numberId = uuid();
-
   render() {
-    console.log('state', this.state.name)
+    // const { filter } = this.state;
+
+    const filteredContacts = this.getFilteredContacts();
+    console.log(filteredContacts);
+    
     return (
       <div className="App">
-        <form onSubmit={this.handleSubmit}>
-          <label htmlFor={this.nameId}>👤 Name
-            <input
-            id={this.nameId}
-            type="text"
-              name="name"
-              value={this.state.name}
-            onChange={this.handleChange}
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
-            required
-            />
-          </label>
-
-          <label htmlFor={this.numberId}>☎️ Number
-            <input
-              id={this.numberId}
-              type="tel"
-              name="number"
-              value={this.state.number}
-              onChange={this.handleChange}
-              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-              maxLength="14"
-              title="номер телефона может быть только в числовом формате"
-              required
-            />
-          </label>
-          <button type="submit" onSubmit={this.handleSubmit}>Add contact</button>
-
-        </form>
         <Container>
-            <ContactList
-            contacts={this.state.contacts}
-            />
+         <h1>Phonebook</h1>
+        <ContactForm addNewContacts={this.addNewContacts}
+        />
         </Container>
 
-
+        <Container>
+          <h2>Contacts</h2>
+            <Filter value={this.state.filter} onChange={ this.filterName}/>
+          <ContactList
+            contacts={filteredContacts}
+            onDeleteBtn={this.deleteContact}
+            />
+        </Container>
       </div>
     )
   }
--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import s from './ContactList.module.css';
 
-const ContactList = ({ contacts }) => {
+const ContactList = ({ contacts, onDeleteBtn }) => {
   return (
-      <ul className={s.list}>{contacts.map(({ id, name, number }) => (
-    <li key={id} className={s.item}>
-      <p className={s.text}>{name}</p>
-      <p className={s.text}>{number}</p>
-    </li>
-  ))}
- 
-  </ul>
+    <ul className={s.list}>{contacts.map( contact => {
+      return (
+        <li key={contact.id} className={s.item}>
+          <p className={s.text}>{contact.name}</p>
+          <p className={s.text}>{contact.number}</p>
+          <button className={s.deleteBtn} type="button" id={contact.id} onClick={() => onDeleteBtn(contact.id)} >Delete ❌</button>
+        </li>);
+      })}
+    </ul>
   )
 };
+
+ContactList.propTypes = {
+  contacts: PropTypes.array.isRequired,
+  onDeleteBtn: PropTypes.func.isRequired,
+}
 
 export default ContactList;
